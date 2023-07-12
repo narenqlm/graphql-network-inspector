b671071ed410960ff8615e7905c8e2fd628d6497
Naren Gogineni <92291785+narenqlm@users.noreply.github.com>
2023-07-13 01:18:13 +0530

    logging

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -1,27 +1,34 @@
-import { useState } from "react"
-import { SplitPaneLayout } from "@/components/Layout"
-import { useNetworkMonitor } from "@/hooks/useNetworkMonitor"
-import { useSearch } from "@/hooks/useSearch"
-import { useNetworkTabs } from "@/hooks/useNetworkTabs"
-import { NetworkPanel } from "../NetworkPanel"
-import { SearchPanel } from "../SearchPanel"
-import { SupportPopover } from "../../components/SupportPopover"
+import {useState} from "react"
+import {SplitPaneLayout} from "@/components/Layout"
+import {useNetworkMonitor} from "@/hooks/useNetworkMonitor"
+import {useSearch} from "@/hooks/useSearch"
+import {useNetworkTabs} from "@/hooks/useNetworkTabs"
+import {NetworkPanel} from "../NetworkPanel"
+import {SearchPanel} from "../SearchPanel"
+import {SupportPopover} from "../../components/SupportPopover"
 
 export const Main = () => {
-  const [selectedRowId, setSelectedRowId] = useState<string | number | null>(
-    null
-  )
-  const [networkRequests, clearWebRequests] = useNetworkMonitor()
-  const { isSearchOpen } = useSearch()
-  const { setActiveTab } = useNetworkTabs()
+    const [selectedRowId, setSelectedRowId] = useState<string | number | null>(
+        null
+    )
+    const [networkRequests, clearWebRequests] = useNetworkMonitor()
+    const {isSearchOpen} = useSearch()
+    const {setActiveTab} = useNetworkTabs()
 
     const onDownload = () => {
-      const transformed: any = []
-        networkRequests.forEach((item)=>transformed.push({operationName: item.request.primaryOperation.operationName, time: item.time, status: item.status}))
-        saveTemplateAsFile("test", transformed)
+        const transformed: any = []
+        const requestList: string[] = []
+        networkRequests.forEach((item) => {
+            const operationName = item.request.primaryOperation.operationName
+            if (!requestList.includes(operationName)) {
+                requestList.push(operationName)
+                transformed.push({operationName, time: item.time, status: item.status})
+            }
+        })
+        saveTemplateAsFile("graphql_request_log", transformed)
     }
     const saveTemplateAsFile = (filename: string, dataObjToWrite: any) => {
-        const blob = new Blob([JSON.stringify(dataObjToWrite)], { type: "text/json" });
+        const blob = new Blob([JSON.stringify(dataObjToWrite)], {type: "text/json"});
         const link = document.createElement("a");
 
         link.download = filename;
@@ -38,31 +45,31 @@ export const Main = () => {
         link.remove()
     };
 
-  return (
-    <>
-      <SplitPaneLayout
-        leftPane={
-          isSearchOpen ? (
-            <SearchPanel
-              networkRequests={networkRequests}
-              onResultClick={(searchResult, networkTab) => {
-                setSelectedRowId(searchResult.networkRequest.id)
-                setActiveTab(networkTab)
-              }}
+    return (
+        <>
+            <SplitPaneLayout
+                leftPane={
+                    isSearchOpen ? (
+                        <SearchPanel
+                            networkRequests={networkRequests}
+                            onResultClick={(searchResult, networkTab) => {
+                                setSelectedRowId(searchResult.networkRequest.id)
+                                setActiveTab(networkTab)
+                            }}
+                        />
+                    ) : undefined
+                }
+                rightPane={
+                    <NetworkPanel
+                        networkRequests={networkRequests}
+                        clearWebRequests={clearWebRequests}
+                        selectedRowId={selectedRowId}
+                        setSelectedRowId={setSelectedRowId}
+                        onDownload={onDownload}
+                    />
+                }
             />
-          ) : undefined
-        }
-        rightPane={
-          <NetworkPanel
-            networkRequests={networkRequests}
-            clearWebRequests={clearWebRequests}
-            selectedRowId={selectedRowId}
-            setSelectedRowId={setSelectedRowId}
-            onDownload={onDownload}
-          />
-        }
-      />
-      <SupportPopover />
-    </>
-  )
+            <SupportPopover/>
+        </>
+    )
 }
